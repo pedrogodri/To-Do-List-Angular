d7f1f7fc57deafda2474a83a5fb436d1f54cee80
Pedro Henrique Godri <pedro.g2006@aluno.ifsc.edu.br>
2023-07-19 14:03:57 -0300

    TaskList

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+
+  public taskList:Array<TaskList> = [
+    {task: "Minha nova Task", checked: true},
+    {task: "Minha nova Task 2", checked: false},
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
 
 }
